70ce8a559d54382792daf440eb5424bdecc6527e
Roxy Khalilifar <roxykhal@hotmail.com>
2025-02-18 17:48:44 +0000

    add xml definition to xml file

--- src/main/java/com/khalilifar/App.java
+++ src/main/java/com/khalilifar/App.java
@@ -7,7 +7,7 @@ public class App
     public static void main( String[] args )
     {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext();
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         Alien obj = (Alien) context.getBean("alien");
         obj.code();
 
